67aad8f061fee791739bb6bfeb8b20ab1841d037
Dan Polityka <73758508+benochi@users.noreply.github.com>
2024-09-16 16:25:40 -0500

    first commit

---
+++ vcs.py
@@ -0,0 +1,21 @@
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import os.path
+
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def authenticate_google_drive():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return build('drive', 'v3', credentials=creds)
